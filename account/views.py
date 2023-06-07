541643620ea5720144f48be1fe17b2f5446b3153
Ali Mirzaei <alimirzaei883@gmail.com>
2023-06-07 23:35:19 +0430

    cmplt register and url template

--- account/views.py
+++ account/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 
 def login_view(request):
     """ user login operation """
     if request.user.is_authenticated:
         # no login form if user is authenticated
-        return redirect('home')
+        return redirect('home:home_url')
 
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -16,7 +17,7 @@ def login_view(request):
         if user is not None:
             login(request, user)
             print("done successfully")
-            return redirect('home')
+            return redirect('home:home_url')
 
     # render the login form if request is not POST.
     return render(request, 'account/login.html')
@@ -25,9 +26,30 @@ def login_view(request):
 def logout_view(request):
     """ log out user """
     logout(request)
-    return redirect('home')
+    return redirect('home:home_url')
 
 
 def register_view(request):
     """ user registration operation """
+    context = {'errors': []}
+    if request.user.is_authenticated:
+        # no login form if user is authenticated
+        return redirect('home:home_url')
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+
+        # error handling
+        if password1 != password2:
+            context['errors'].append('passwords does not match!')
+            return render(request, 'account/register.html', context)
+
+        user = User.objects.create(username=username, password=password1, email=email)
+        # login user after registration
+        login(request, user)
+        return redirect('home:home_url')
+
     return render(request, 'account/register.html')
