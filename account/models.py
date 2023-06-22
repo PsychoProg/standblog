9c8a604767ca12c9380a2c1e61e55d5e27d08aa0
Ali Mirzaei <alimirzaei883@gmail.com>
2023-06-22 12:28:45 +0430

    default user image

--- account/models.py
+++ account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    father_name = models.CharField(max_length=30)
+    national_code = models.CharField(max_length=10)
+    user_image = models.ImageField(upload_to='profiles/images', db_column='image', null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
